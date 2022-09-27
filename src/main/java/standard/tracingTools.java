0c4ee606a254db2ead6284de91816c5c650fda1e
Lars Schluchter <lars.schluchter@students.fhnw.ch>
2022-09-27 08:49:17 +0200

    Add CreateEyeRay

--- src/main/java/standard/tracingTools.java
+++ src/main/java/standard/tracingTools.java
@@ -5,11 +5,19 @@ import vectors.Vector3;
 
 public class tracingTools {
 
-    public Vector3 CreateEyeRay(Vector3 eye, Vector3 lookAt, float fov, Vector2 Pixel) {
+    public Vector3 CreateEyeRay(Vector3 eye, Vector3 lookAt, float fov, Vector2 pixel) {
 
+        Vector3 up = new Vector3(0,0,1);
+        Vector3 f = lookAt.subtract(eye);
+        Vector3 r = Vector3.cross(up, f);
+        Vector3 u = Vector3.cross(f, r);
+        Vector3 fNorm = Vector3.normalize(f);
+        Vector3 rNorm = Vector3.normalize(r);
+        Vector3 uNorm = Vector3.normalize(u);
+        Vector3 temp1 = rNorm.multiply((float)(Math.tan(fov/2)* pixel.x()));
+        Vector3 temp2 = uNorm.multiply((float)(Math.tan(fov/2)* pixel.y()));
 
-
-        return new Vector3(0,0,0);
+        return fNorm.add(temp1).add(temp2);
     }
 
 
